8cf2ceca5446d3bd7e0cc0becc601cf52587529a
Your Name <you@example.com>
2021-08-16 18:15:45 -0400

    SavedBooks.js progress

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -2,15 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
-
 import { useMutation } from '@apollo/react-hooks';
 import { SAVE_BOOK } from '...utils/mutations';
-import { removeBookId } from '../utils/localStorage';
 
 const SearchBooks = () => {
-
+  //"useMutation()"" Hook which executes the "SAVE_BOOK" mutation in the "handleDeleteBook()" function
   const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // create state for holding returned google api data
@@ -77,7 +74,7 @@ const SearchBooks = () => {
       });
       console.log(data);
 
-      if (!response.ok) {
+      if (error) {
         throw new Error('something went wrong!');
       }
       // if book successfully saves to user's account, save book id to state
