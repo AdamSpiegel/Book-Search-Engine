c9ca9491cce99cf23c42242d5b14a6d3808df88d
Your Name <you@example.com>
2021-08-17 02:16:23 -0400

    Server.js bug fixes

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const db = require('./config/connection');
-const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
 const app = express();
@@ -19,17 +18,21 @@ const server = new ApolloServer({
 // Uses Apollo server features by updating via Express.js
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  })
 }
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
