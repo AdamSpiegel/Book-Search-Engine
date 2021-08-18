34084677b9b00efe4f27d184fc51ea0403fd0621
Your Name <you@example.com>
2021-08-18 10:26:41 -0400

    Deploying to Heroku

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Book_Search_Engine', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/BookSearchEngine', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
